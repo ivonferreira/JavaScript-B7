6a3b7d0e4b7a28dd9bebaff14b02b07ff4c9cf9c
Ivon Ferreira <ivonferreira@users.noreply.github.com>
2023-03-02 10:57:48 -0300

    atualizando

---
+++ Mod09/Aula02/script.js
@@ -0,0 +1,9 @@
+let nome = "Ivon Ferreira Lins Neto"
+//nome.length
+//nome.indexOf("string")
+//nome.slice
+//nome.substring
+//nome.substr
+let resultado = nome.substr(4,10);
+
+console.log(resultado)
---
+++ Mod09/Aula03/script.js
@@ -0,0 +1,17 @@
+let nome = "Ivon Ferreira Lins Neto"
+//nome.length
+//nome.indexOf("string")
+//nome.slice(-10,-4)
+//nome.substring(1,10)
+//nome.substr(5,3)
+//nome.replace("atual","substituto")
+//nome.toUpperCase()
+//nome.toLowerCase()
+//nome.concat("juntar")
+//nome.trim()
+//nome.charAt()
+//nome.split("")
+let resultado = nome.split(" ");
+
+console.log(nome)
+console.log(resultado)
