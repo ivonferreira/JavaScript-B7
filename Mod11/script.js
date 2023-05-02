2777270025cc701470cdffa3c36a985db724af92
Ivon Ferreira <ivonferreira@users.noreply.github.com>
2023-05-02 17:02:58 -0300

    Atualizando Projeto

--- Mod11/script.js
+++ Mod11/script.js
@@ -1,9 +1,12 @@
+let modalQt = 1;
+
 const c = (el)=>document.querySelector(el);// Funcão para reduzir codigo transformando documente.querySelector na variavel c.
 const cs =(el)=>document.querySelectorAll(el);// querySelectorAll em cs
 
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
@@ -11,6 +14,22 @@ pizzaJson.map((item, index)=>{
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('a').addEventListener('click',(e)=>{
         e.preventDefault(); // Evita que faça a função Default de atualizar a pagina
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if(sizeIndex ==2){
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+        });
+
+        c('.pizzaInfo--qt').innerHTML = modalQt;
 
         c('.pizzaWindowArea').style.opacity = 0;// Opacidade 0 Para criar animação
         c('.pizzaWindowArea').style.display = 'flex';//Aparece a janela modal
@@ -20,3 +39,14 @@ pizzaJson.map((item, index)=>{
     
     c('.pizza-area').append(pizzaItem);
 });
+
+// Eventos do modal
+function closeModal(){
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    },500);
+}
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+})
